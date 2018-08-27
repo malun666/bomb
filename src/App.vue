cde2153d01825d169c7502f86e2438ad611f68a9
malun <malun666@126.com>
2018-08-27 23:54:17 +0800

    :sparkles: added the bomb table component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layout">
+      <div class="main">
+        <mine-table :rows="rows" :cell-height="cellHeight" :cell-width="cellWidth" :cols="columns"></mine-table>      
+      </div>
+      <div class="aside">
+        <div class="block">
+          <span class="demonstration">列数：{{columns}}</span>
+          <el-slider :min="1" v-model="columns"></el-slider>
+        </div>
+        <div class="block">
+          <span class="demonstration">行数：{{rows}}</span>
+          <el-slider :min="1" v-model="rows"></el-slider>
+        </div>
+        <div class="block">
+          <span class="demonstration">单元格宽：{{cellWidth}}</span>
+          <el-slider :min="1" v-model="cellWidth"></el-slider>
+        </div>
+        <div class="block">
+          <span class="demonstration">单元格高度：{{cellHeight}}</span>
+          <el-slider :min="1" v-model="cellHeight"></el-slider>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+import MineTable from "./components/MineTable";
+export default {
+  name: "app",
+  data: function() {
+    return {
+      rows: 10,
+      columns: 10,
+      cellWidth: 30,
+      cellHeight: 30
+    };
+  },
+  components: {
+    "mine-table": MineTable
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .layout {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+  .layout > div {
+    min-height: 300px;
+    border: 1px solid #c09;
+    box-sizing: border-box;
+  }
+  .main {
+    flex: 5 0 500px;
+  }
+  .aside {
+    flex: 1 0 100px;
   }
 }
 </style>
---
+++ src/components/MineTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mine">
+    <table>
+      <tbody>
+        <tr v-for="item of rows" :key="'row_' + item">
+          <td :style="{width: cellWidth+'px', height: cellHeight + 'px'}" v-for="colItem of cols" :key="'col_' + colItem"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mine-tbale",
+  data() {
+    return {
+      bomArray: []
+    };
+  },
+  created() {},
+  props: {
+    rows: {
+      type: Number,
+      required: true
+    },
+    cols: {
+      type: Number,
+      required: true
+    },
+    cellHeight: {
+      type: Number,
+      required: true,
+      default: 30
+    },
+    cellWidth: {
+      type: Number,
+      required: true,
+      default: 30
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid #c09;
+  height: 30px;
+  width: 30px;
+  background-color: aliceblue;
+}
+</style>
